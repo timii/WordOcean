6f25f9c78dfd83d53b019bc2a9a7c11e5cb2d521
Tim Thomas <tim-t@hotmail.de>
2021-04-30 23:32:54 +0200

    refactor header placements

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ function Header() {
   return (
     <div className="header">
       <div className="header_text">Word Ocean</div>
-      <DarkModeToggle className="header_toggle"/>
+      <div className="header_subtext">Lookup word definitions and additional infomation</div>
+      <DarkModeToggle />
     </div>
   );
 }
