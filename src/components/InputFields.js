dd8100744a4ecad8e2a64b8918acf59dd9903915
Tim Thomas <tim-t@hotmail.de>
2021-05-21 16:54:53 +0200

    replace search icon

--- src/components/InputFields.js
+++ src/components/InputFields.js
@@ -66,11 +66,9 @@ function InputFields(props) {
           onChange={(e) => props.onWordChanged(e.target.value)}
         ></input>
         <div className="search_container">
+          {/* <button onClick={props.handleSubmit} className="input_search">Search</button> */}
           <input type="submit" value="Search" className="input_search" />
-          <svg width="30" height="30" fill="none" viewBox="0 0 24 24">
-            <circle cx="11" cy="11" r="6.25" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></circle>
-            <path fill="currentColor" d="M18.7197 19.7803C19.0126 20.0732 19.4874 20.0732 19.7803 19.7803C20.0732 19.4874 20.0732 19.0126 19.7803 18.7197L18.7197 19.7803ZM14.9697 16.0303L18.7197 19.7803L19.7803 18.7197L16.0303 14.9697L14.9697 16.0303Z"></path>
-          </svg>
+          <div className="search_icon"></div>
         </div>
       </form>
     </div>
