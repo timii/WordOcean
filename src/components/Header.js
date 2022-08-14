d8b02487f1b5efc1a98b546e0c449304e1862f02
Tim Thomas <tim-t@hotmail.de>
2022-08-14 21:47:38 +0200

    feat: add github repo link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ import DarkModeToggle from "./DarkModeToggle";
 function Header() {
   return (
     <div className="header">
+      <a href="https://github.com/timii/WordOcean" target="_blank" rel="noreferrer">
+        <button className="header_github"></button>
+      </a>
       <div className="header_text">Word Ocean</div>
-      {/* <div className="header_subtext">Lookup word definitions and additional infomation</div> */}
       <DarkModeToggle />
     </div>
   );
